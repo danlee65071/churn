3bcd392fa558a72169d4c49756b0d2c081dc3f42
Daniil <danlee65071@gmail.com>
2022-09-25 19:06:56 +0300

    utils

--- utils.py
+++ utils.py
@@ -4,3 +4,7 @@ import zipfile
 def extract(path_zip: str, dir_extract: str):
     with zipfile.ZipFile(path_zip, 'r') as zip_ref:
         zip_ref.extractall(dir_extract)
+
+
+def mkdir(dir_name: str):
+    
